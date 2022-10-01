f73d35efaef496aee7b3c82a26f1e241b5c406d3
ggslz <76657220+ggslz@users.noreply.github.com>
2022-10-01 12:20:33 -0300

    Update perimeter_and_mode_control.py

--- perimeter_and_mode_control.py
+++ perimeter_and_mode_control.py
@@ -16,11 +16,6 @@ def connectMyCopter():
 	
 	connection_string =args.connect
 
-	if not connection_string:
-		import dronekit_sitl
-		sitl= dronekit_sitl.start_default()
-		connection_string = sitl.connection_string()
-
 
 	vehicle = connect(connection_string, wait_ready =True)
 
