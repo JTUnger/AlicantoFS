ead7ad7e71fe6c6bd2f1297a5560acf1134eead4
ggslz <76657220+ggslz@users.noreply.github.com>
2022-10-01 15:06:30 -0300

    Update precision_landing_single_aruco.py

--- precision_landing_single_aruco.py
+++ precision_landing_single_aruco.py
@@ -33,10 +33,11 @@ cap = WebcamVideoStream(src=0, width=horizontal_res, height=vertical_res).start(
 horizontal_fov = 59 * (math.pi / 180 ) ##Pi cam V1: 53.5 V2: 62.2
 vertical_fov = 46 * (math.pi / 180)    ##Pi cam V1: 41.41 V2: 48.8
 
-calib_path="/home/pi/video2calibration/calibrationFiles/"
-cameraMatrix   = np.loadtxt(calib_path+'cameraMatrix.txt', delimiter=',')
-cameraDistortion   = np.loadtxt(calib_path+'cameraDistortion.txt', delimiter=',')
-##
+
+cameraMatrix  = np.array([[933.9466925521707, 0, 358.59608943398365],
+                [0, 935.0635463990791, 293.0721064675127],
+                [0, 0, 1]])
+cameraDistortion   = np.array([-0.4530790972005633, 0.3951099938612813, 0.0037673873203789916, 0.0016363264710513889, -0.38177331299300393])
 
 ##Counters and script triggers
 found_count=0
