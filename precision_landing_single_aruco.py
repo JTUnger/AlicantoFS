c915d545cb6784dc1b5f0e442e4639e33ff3cc96
ggslz <76657220+ggslz@users.noreply.github.com>
2022-10-01 15:28:43 -0300

    Update precision_landing_single_aruco.py

--- precision_landing_single_aruco.py
+++ precision_landing_single_aruco.py
@@ -26,18 +26,18 @@ parameters = aruco.DetectorParameters_create()
 ##
 
 ##Camera
-horizontal_res = 640
-vertical_res = 480
+horizontal_res = 800
+vertical_res = 600
 cap = WebcamVideoStream(src=0, width=horizontal_res, height=vertical_res).start()
 
-horizontal_fov = 59 * (math.pi / 180 ) ##Pi cam V1: 53.5 V2: 62.2
-vertical_fov = 46 * (math.pi / 180)    ##Pi cam V1: 41.41 V2: 48.8
+horizontal_fov =59  * (math.pi / 180 ) ##Pi cam V1: 53.5 V2: 62.2
+vertical_fov =46  * (math.pi / 180)    ##Pi cam V1: 41.41 V2: 48.8
 
-
-cameraMatrix  = np.array([[933.9466925521707, 0, 358.59608943398365],
+cameraMatrix   = np.array([[933.9466925521707, 0, 358.59608943398365],
                 [0, 935.0635463990791, 293.0721064675127],
                 [0, 0, 1]])
 cameraDistortion   = np.array([-0.4530790972005633, 0.3951099938612813, 0.0037673873203789916, 0.0016363264710513889, -0.38177331299300393])
+##
 
 ##Counters and script triggers
 found_count=0
@@ -95,7 +95,7 @@ def arm_and_takeoff(targetHeight):
 	vehicle.simple_takeoff(targetHeight) ##meters
 
 	while True:
-		print("Current Altitude: %d"%vehicle.location.global_relative_frame.alt)
+		print(("Current Altitude: %d"%vehicle.location.global_relative_frame.alt))
 		if vehicle.location.global_relative_frame.alt>=.95*targetHeight:
 			break
 		time.sleep(1)
@@ -178,15 +178,15 @@ def lander():
             else:
                 send_land_message(x_ang,y_ang)
                 pass
-            print("X CENTER PIXEL: "+str(x_avg)+" Y CENTER PIXEL: "+str(y_avg))
-            print("FOUND COUNT: "+str(found_count)+" NOTFOUND COUNT: "+str(notfound_count))
-            print("MARKER POSITION: x=" +x+" y= "+y+" z="+z)
+            print(("X CENTER PIXEL: "+str(x_avg)+" Y CENTER PIXEL: "+str(y_avg)))
+            print(("FOUND COUNT: "+str(found_count)+" NOTFOUND COUNT: "+str(notfound_count)))
+            print(("MARKER POSITION: x=" +x+" y= "+y+" z="+z))
             found_count=found_count+1
             print("")
-		else:
+        else:
             notfound_count=notfound_count+1
     except Exception as e:
-        print('Target likely not found. Error: '+str(e))
+        print(('Target likely not found. Error: '+str(e)))
         notfound_count=notfound_count+1
     
      
@@ -210,14 +210,14 @@ vehicle.parameters['LAND_SPEED'] = 20 ##Descent speed of 30cm/s
 
 if script_mode ==1:
     arm_and_takeoff(takeoff_height)
-    print(str(time.time()))
-#send_local_ned_velocity(velocity,velocity,0) ##Offset drone from target
+    print((str(time.time())))
+    #send_local_ned_velocity(velocity,velocity,0) ##Offset drone from target
     time.sleep(1)
-ready_to_land=1
+    ready_to_land=1
 elif script_mode==2:
     while vehicle.mode!='GUIDED':
         time.sleep(1)
-    print("Waiting for manual change from mode "+str(vehicle.mode)+" to GUIDED")
+        print(("Waiting for manual change from mode "+str(vehicle.mode)+" to GUIDED"))
     ready_to_land=1
 
 if ready_to_land==1:
@@ -229,10 +229,10 @@ if ready_to_land==1:
 
     total_count=found_count+notfound_count
     freq_lander=total_count/total_time
-    print("Total iterations: "+str(total_count))
-    print("Total seconds: "+str(total_time))
+    print(("Total iterations: "+str(total_count)))
+    print(("Total seconds: "+str(total_time)))
     print("------------------")
-    print("lander function had frequency of: "+str(freq_lander))
+    print(("lander function had frequency of: "+str(freq_lander)))
     print("------------------")
     print("Vehicle has landed")
     print("------------------")
