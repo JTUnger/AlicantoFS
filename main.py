027ed146e2bc461db82aa52b8669ecf43d3d42f5
JTUnger <josetomas.cl@gmail.com>
2022-10-22 20:03:47 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ import time
 
 vehicle = connectMyCopter()
 arm_takeoff(vehicle, 4)
-aruco_precision_landing(vehicle, start_height=5, safety_height= 10)
+aruco_precision_landing(vehicle, start_height=6, safety_height= 12)
