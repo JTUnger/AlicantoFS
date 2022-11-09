79f4d8daa6827800ae88c1647907d0596d0579ae
JTUnger <josetomas.cl@gmail.com>
2022-11-09 08:26:09 -0300

    update on cv_func and SAR_main

---
+++ SAR_main.py
@@ -0,0 +1,34 @@
+from flight_functions import *
+from cv_functions import *
+from funciones_lora import *
+import exceptions
+import time 
+
+#Task parameters and field dimensions
+takeoff_height = 8
+x_area = 10
+y_area = 10
+x_landpad = 1
+y_landpad = 1
+search_height = 12
+horizontal_res = 1920
+vertical_res = 1080
+doRTL = True
+debug = False
+
+#Connect to vehicle and return Vehicle object
+vehicle = connectMyCopter()
+
+#Arm vehicle and takeoff to specified height
+arm_takeoff(vehicle, takeoff_height)
+
+#Run SAR search pattern and grab images and GPS coordinates (Se que se ve horrible, pero es lo que hay)
+(photo1, photo2,
+photo1_lat, photo1_long, 
+photo2_lat, photo2_long, 
+home_lat, home_long) = SAR_search_pattern(vehicle, x_area, y_area, x_landpad, y_landpad, 
+                                            search_height, horizontal_res, vertical_res, 
+                                                doRTL, debug)
+
+#Use OpenCV to process images and return the coordinates of the target
+pass
