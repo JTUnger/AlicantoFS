bc80b2fee4973a3b35fd6e8c50c30e6501e02fe5
JTUnger <josetomas.cl@gmail.com>
2022-11-13 10:24:31 +1100

    added calib files

---
+++ DebugTools/imageBW_taker.py
@@ -0,0 +1,21 @@
+from flight_functions import *
+from picamera import PiCamera
+from time import sleep
+
+photo_id = 1
+
+camera = PiCamera()
+camera.resolution = (1920, 1080)
+camera.color_effects = (128,128)
+camera.start_preview()
+sleep(2)
+
+while True:
+    #ret, frame = cap.read()
+    #Save to images folder with id 
+    save_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "images")
+    save_name = "photo_" + str(photo_id) + ".jpg"
+    camera.capture(os.path.join(save_path, save_name))
+    print("Photo saved: " + save_name)
+    photo_id += 1
+    time.sleep(0.7)
