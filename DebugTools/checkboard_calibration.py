1ff44d2fd144f23cc1b0808b82a25b5a4ef3a7b0
JTUnger <josetomas.cl@gmail.com>
2022-11-13 11:17:23 +1100

    fix checkboard

--- DebugTools/checkboard_calibration.py
+++ DebugTools/checkboard_calibration.py
@@ -1,68 +1,54 @@
- 
-import cv2
 import numpy as np
-import os
+import cv2 as cv
 import glob
- 
-# Defining the dimensions of checkerboard
-CHECKERBOARD = (6,9)
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
- 
-# Creating vector to store vectors of 3D points for each checkerboard image
+import json
+from datetime import datetime
+
+criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+rows = 6
+columns = 9
+objp = np.zeros((rows*columns,3), np.float32)
+objp[:,:2] = np.mgrid[0:columns,0:rows].T.reshape(-1,2)
+
 objpoints = []
-# Creating vector to store vectors of 2D points for each checkerboard image
-imgpoints = [] 
- 
- 
-# Defining the world coordinates for 3D points
-objp = np.zeros((1, CHECKERBOARD[0] * CHECKERBOARD[1], 3), np.float32)
-objp[0,:,:2] = np.mgrid[0:CHECKERBOARD[0], 0:CHECKERBOARD[1]].T.reshape(-1, 2)
-prev_img_shape = None
- 
-# Extracting path of individual image stored in a given directory
-images = glob.glob('./images/*.jpg')
+imgpoints = []
+
+#Search for images in the folder
+images = glob.glob('images/*.jpg')
+print(len(images), "images found")
+
 for fname in images:
-    img = cv2.imread(fname)
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    # Find the chess board corners
-    # If desired number of corners are found in the image then ret = true
-    ret, corners = cv2.findChessboardCorners(gray, CHECKERBOARD, cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_FAST_CHECK + cv2.CALIB_CB_NORMALIZE_IMAGE)
-     
-    """
-    If desired number of corner are detected,
-    we refine the pixel coordinates and display 
-    them on the images of checker board
-    """
+    img = cv.imread(fname)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+    chessboard_flags = cv.CALIB_CB_ADAPTIVE_THRESH + cv.CALIB_CB_FAST_CHECK + cv.CALIB_CB_NORMALIZE_IMAGE
+    ret, corners = cv.findChessboardCorners(gray, (columns,rows), chessboard_flags)
+
     if ret == True:
         objpoints.append(objp)
-        # refining pixel coordinates for given 2d points.
-        corners2 = cv2.cornerSubPix(gray, corners, (11,11),(-1,-1), criteria)
-         
-        imgpoints.append(corners2)
- 
-        # Draw and display the corners
-        img = cv2.drawChessboardCorners(img, CHECKERBOARD, corners2, ret)
-     
-    cv2.imshow('img',img)
-    cv2.waitKey(0)
- 
-cv2.destroyAllWindows()
- 
-h,w = img.shape[:2]
- 
-"""
-Performing camera calibration by 
-passing the value of known 3D points (objpoints)
-and corresponding pixel coordinates of the 
-detected corners (imgpoints)
-"""
-ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
- 
-print("Camera matrix : \n")
-print(mtx)
-print("dist : \n")
-print(dist)
-print("rvecs : \n")
-print(rvecs)
-print("tvecs : \n")
-print(tvecs)
+        corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
+        imgpoints.append(corners)
+
+        cv.drawChessboardCorners(img, (columns,rows), corners2, ret)
+        cv.imshow('img', img)
+        cv.imwrite(f"chessboard_corners_{datetime.now().strftime('%Y%m%d_%H%M%S')}.png", img)
+        cv.waitKey(1500)
+
+ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+
+camera = {}
+
+class NumpyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
+
+for variable in ['ret', 'mtx', 'dist', 'rvecs', 'tvecs']:
+    camera[variable] = eval(variable)
+
+with open("camera.json", 'w') as f:
+    json.dump(camera, f, indent=4, cls=NumpyEncoder)
+
+cv.destroyAllWindows()
